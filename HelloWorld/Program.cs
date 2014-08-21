740d7e436ed6df8a71217c91f0357d9390154023
jackvon13 <jack.von13@gmail.com>
2014-08-21 16:51:35 -0500

    Modificado por Albert

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,6 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("JackVon13: Welcome to my world!");
+            //Console.WriteLine("JackVon13: Welcome to my world!");
         }
     }
 }
